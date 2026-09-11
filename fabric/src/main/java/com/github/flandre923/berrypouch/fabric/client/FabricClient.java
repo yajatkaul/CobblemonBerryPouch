1d95edbc8b869ff97669f4aa8396eac914850547
yajatkaul <mc.crafter1973@gmail.com>
2026-09-11 14:46:38 +0530

    My bad

--- fabric/src/main/java/com/github/flandre923/berrypouch/fabric/client/FabricClient.java
+++ fabric/src/main/java/com/github/flandre923/berrypouch/fabric/client/FabricClient.java
@@ -1,20 +1,3 @@
-/*
- * This file is part of Radical Cobblemon Trainers.
- * Copyright (c) 2025, HDainester, All rights reserved.
- *
- * Radical Cobblemon Trainers is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Radical Cobblemon Trainers is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
- * more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with Radical Cobblemon Trainers. If not, see <http://www.gnu.org/licenses/lgpl>.
- */
 package com.github.flandre923.berrypouch.fabric.client;
 
 import com.github.flandre923.berrypouch.ModClientCommon;
@@ -37,6 +20,5 @@ public class FabricClient implements ClientModInitializer {
         MenuScreens.register(ModRegistries.ModMenuTypes.APRICORN_BASKET_MENU.get(), ApricornBasketScreen::new);
         KeyBindingManager.register();
         ModClientCommon.init();
-
     }
 }
