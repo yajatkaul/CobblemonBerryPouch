4a3e1cb130863374b99248625079b47419e3d9fc
yajatkaul <mc.crafter1973@gmail.com>
2026-09-11 12:12:14 +0530

    0.6.1 fixing

--- neoforge/src/main/java/com/github/flandre923/berrypouch/neoforge/InventorySorterIntegration.java
+++ neoforge/src/main/java/com/github/flandre923/berrypouch/neoforge/InventorySorterIntegration.java
@@ -8,7 +8,7 @@ import net.neoforged.fml.InterModComms;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
 
-@EventBusSubscriber(modid = ModCommon.MOD_ID,bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = ModCommon.MOD_ID)
 public class InventorySorterIntegration {
 
 	@SubscribeEvent
